f44ee91adeac23b5442efe0987019f1ce8c5382a
José <josebarrientos.rodriguez@gmail.com>
2026-05-04 16:03:48 -0400

    Arregla config de ENV

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
-    wpApiUrl: process.env.WP_API_URL,
-    wpApiUser: process.env.WP_API_USER,
-    wpApiPass: process.env.WP_API_PASS,
-    wpSsoSecret: process.env.SSO_SECRET,
+    // Estas se pueden sobreescribir en producción con NUXT_WP_API_URL, etc.
+    wpApiUrl: process.env.WP_API_URL || '',
+    wpApiUser: process.env.WP_API_USER || '',
+    wpApiPass: process.env.WP_API_PASS || '',
+    wpSsoSecret: process.env.SSO_SECRET || '',
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
     }
